8f0a3467d6733ce8b59137b72e3fe9720fd4f6a6
Mark Seeliger <117680637+mkslge@users.noreply.github.com>
2025-04-16 11:57:44 -0500

    Create loadtestpost.js

---
+++ loadtestpost.js
@@ -0,0 +1,32 @@
+import http from 'k6/http';
+import {sleep } from 'k6';
+
+
+
+//grab from cli or use default values
+let address = __ENV.ADDRESS || 'http://192.168.51.110:14141';
+let virtualUsers = Number(__ENV.VUS) || 10;
+let duration = __ENV.DURATION || '30s';
+
+export let options = {
+    vus: virtualUsers, //virtual users
+    duration: duration, //duration
+}
+
+//test data
+export default function () {
+    const payload = JSON.stringify({
+        username: 'testuser',
+        password: 'supersecurepassword'
+    });
+
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    http.post(address, payload, params);
+    sleep(1);
+}
+
